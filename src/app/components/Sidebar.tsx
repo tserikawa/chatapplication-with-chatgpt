78b0a112894f7cd5be2b7ae3c5b7973f41234a2e
tserikawa <tkt.srkw@gmail.com>
2025-01-25 08:06:07 +0900

    20250125

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  where,
+  query,
+  Timestamp,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { IoIosLogOut } from "react-icons/io";
+import { db } from "../../../firebase";
+
+type Room = {
+  id: string;
+  name: string;
+  createdAt: Timestamp;
+};
 
 const Sidebar = () => {
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  useEffect(() => {
+    const fetchRooms = async () => {
+      const roomCollectionRef = collection(db, "rooms");
+      const q = query(
+        roomCollectionRef,
+        where("userId", "==", "wufFXF8qq3aWDedsfDe1Z2DOiBW2").orderBy(
+          "createdAt"
+        )
+      );
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        const newRooms: Room[] = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          name: doc.data().name,
+          createdAt: doc.data().createdAt,
+        }));
+        setRooms(newRooms);
+        console.log(newRooms);
+      });
+    };
+    fetchRooms();
+  }, []);
+
   return (
     <div className="bg-custom-blue h-full overflow-y-auto px-5 flex flex-col">
       {/* flex-grow　余白を引き延ばす */}
@@ -11,18 +52,14 @@ const Sidebar = () => {
           <h1 className="text-white text-xl font-bold p-4 flex">New Chat</h1>
         </div>
         <ul>
-          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-            Room1
-          </li>
-          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-            Room2
-          </li>
-          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-            Room3
-          </li>
-          <li className="cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150">
-            Room4
-          </li>
+          {rooms.map((room) => (
+            <li
+              key={room.id}
+              className="flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700 duration-150"
+            >
+              <span>{room.name}</span>
+            </li>
+          ))}
         </ul>
       </div>
       <div className="text-xl flex items-center justify-evenly mb-2 cursor-pointer p-4 text-slate-100 hover:bg-slate-700 duration-150">
